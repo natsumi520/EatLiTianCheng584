240b78fc7107eea7220191b8ee3ca2229c8d6f59
华风夏韵-洛水天依 <65113604+natsumi520@users.noreply.github.com>
2022-02-05 23:07:50 +0800

    升级

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html lang="zh">
 
 <head>
-    <title>吃掉小鹿乃</title>
-    <meta itemprop="name" content="吃掉小鹿乃" />
-    <meta itemprop="description" content="新概念音游" />
+    <title>把李天成584吃了！</title>
+    <meta itemprop="name" content="把李天成584吃了！" />
+    <meta itemprop="description" content="新概念阴游" />
     <meta charset="utf-8" />
     <meta name="viewport" content="initial-scale=1, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0, width=device-width,target-densitydpi=device-dpi" />
     <link href="./static/index.css" rel="stylesheet" type="text/css">
@@ -14,7 +14,7 @@
     <link href="https://cdn.staticfile.org/twitter-bootstrap/5.1.1/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.staticfile.org/twitter-bootstrap/5.1.1/js/bootstrap.bundle.min.js"></script>
     <script src="https://libs.baidu.com/jquery/2.0.0/jquery.min.js"></script>
-	<?php
+    <?php
     session_start();
     $str = substr(str_shuffle('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'), 0, 8);
     $_SESSION['t'] = $str;
@@ -30,8 +30,8 @@
             <div id="GameScoreLayer-score" style="margin:10px 0;">得分</div>
             <div id="GameScoreLayer-bast">最佳</div>
             <button type="button" class="btn btn-secondary btn-lg" id="replay" onclick="replayBtn()">重来</button>
-        <button type="button" class="btn btn-secondary btn-lg" onclick="window.location.reload()">主页</button>
-			<button type="button" class="btn btn-secondary btn-lg" onclick="goRank();">排行</button>
+            <button type="button" class="btn btn-secondary btn-lg" onclick="window.location.reload()">主页</button>
+            <button type="button" class="btn btn-secondary btn-lg" onclick="goRank();">排行</button>
             <button type="button" class="btn btn-secondary btn-lg" onclick="window.location.href='https://github.com/arcxingye/EatKano'">开源</button>
         </div>
     </div>
@@ -40,7 +40,7 @@
         <div class="welcome-bg FILL"></div>
         <div class="FILL BOX-M" style="position:absolute;top:0;left:0;right:0;bottom:0;z-index:5;">
             <div style="margin:0 8% 0 9%;">
-                <div style="font-size:2.6em; color:#FEF002;">新概念音游</div><br />
+                <div style="font-size:2.6em; color:#FEF002;">新概念阴游</div><br />
                 <div id="desc" style="display: block;font-size:2.2em; color:#fff; line-height:1.5em;">
                     从最底下的开始<br />
                     看你能得多少分<br />
@@ -48,7 +48,7 @@
                 <br />
                 <div id="btn_group" style="display: block;">
                     <a class="btn btn-primary btn-lg" onclick="readyBtn()">开始游戏</a>
-                    <br/><br/>
+                    <br /><br />
 
                     <div class="dropdown">
                         <a class="btn btn-secondary btn-lg" href="javascript: void(0);" role="button" id="mode" data-bs-toggle="dropdown" aria-expanded="false">普通模式</a>
@@ -59,7 +59,7 @@
                         </ul>
                     </div>
 
-                    <br/>
+                    <br />
                     <a class="btn btn-secondary btn-lg" onclick="show_setting()">游戏设置</a>
                 </div>
                 <div id="setting" style="display: none;">
@@ -73,7 +73,7 @@
                         <div class="input-group-prepend">
                             <span class="input-group-text" id="basic-addon1">标题</span>
                         </div>
-                        <input type="text" id="title" class="form-control" placeholder="吃掉小鹿乃">
+                        <input type="text" id="title" class="form-control" placeholder="把李天成584吃了！">
                     </div>
                     <div class="input-group mb-3">
                         <div class="input-group-prepend">
